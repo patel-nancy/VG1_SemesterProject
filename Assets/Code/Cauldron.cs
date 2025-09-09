6692477acf707673ffc11dac0aa7fb69c2f2c003
patel-nancy <nancy.patel@wustl.edu>
2025-09-09 17:45:57 -0500

    only stir when dragged. TODO: when you click on the spoon, it "jumps" to a different part of the rotation

--- Assets/Code/Cauldron.cs
+++ Assets/Code/Cauldron.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Cauldron : MonoBehaviour
 {
     public Transform cauldronCenter;
+    public Drag spoon;
     
     public List<IngredientName> ingredients = new List<IngredientName>();
     
@@ -22,13 +23,16 @@ public class Cauldron : MonoBehaviour
 
     void Update()
     {
-        Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 directionFromCauldronToMouse = mousePositionInWorld - transform.position;
+        if (spoon.dragging)
+        {
+            Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 directionFromCauldronToMouse = mousePositionInWorld - transform.position;
         
-        float radiansToMouse = Mathf.Atan2(directionFromCauldronToMouse.y, directionFromCauldronToMouse.x);
-        float angleToMouse = radiansToMouse * Mathf.Rad2Deg;
+            float radiansToMouse = Mathf.Atan2(directionFromCauldronToMouse.y, directionFromCauldronToMouse.x);
+            float angleToMouse = radiansToMouse * Mathf.Rad2Deg;
         
-        cauldronCenter.rotation = Quaternion.Euler(0, 0, angleToMouse);
+            cauldronCenter.rotation = Quaternion.Euler(0, 0, angleToMouse);
+        }
     }
     
 }
