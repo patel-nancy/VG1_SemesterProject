20291face980a40cb16837daf265533cfddb8d0c
patel-nancy <nancy.patel@wustl.edu>
2025-09-16 00:32:46 -0500

    random rat spawn! TODO: add footprints when rat comes

---
+++ Assets/Code/PotionMaking/Rat.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rat : MonoBehaviour
+{
+    public bool knocksShelf;
+    private float speed = 2f;
+    private bool moveLeftOrUp = true;
+    
+    private Vector2 knockPosition = new Vector2(4, -1);
+    private Vector2 bellowsPosition = new Vector2(-6.5f, -3);
+
+    void Update()
+    {
+        float going = moveLeftOrUp ? 1 : -1;
+        Vector2 directionVector = knocksShelf ? Vector2.left : Vector2.up;
+        
+        transform.Translate(directionVector * going * speed * Time.deltaTime);
+
+        // reaches edge of shelf or top of bellows, then turns around
+        if ((knocksShelf && transform.position.x < knockPosition.x) || (!knocksShelf && transform.position.y > bellowsPosition.y))
+        {
+            moveLeftOrUp = false;
+        }
+
+        // out of bounds -- destroy
+        if (Mathf.Abs(transform.position.x) > 10 || Mathf.Abs(transform.position.y) > 10) 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
