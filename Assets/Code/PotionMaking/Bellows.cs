40e07042a603ae66a74e16bece1f95c9d403e6c5
Xzez0 <b.ousmane@wustl.edu>
2025-10-31 22:58:02 -0500

    Added crackling fire for the bellow
    Used AudioSource and the audio library to add fire crackling sounds when the bellow is pulled down.

--- Assets/Code/PotionMaking/Bellows.cs
+++ Assets/Code/PotionMaking/Bellows.cs
@@ -9,25 +9,62 @@ public class Bellows : MonoBehaviour
 
     private Vector2 offset;
     private float stopYPosition = -0f;
+
+    public AudioSource audioSource;
+    public AudioClip fireCrackleSound;
+
     
+
+      void Start()
+    {
+        // auto get the AudioSource on this GameObject if not assigned
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+
+
     private void OnMouseDrag()
     {
         Vector2 mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 currentPositionInWorld = transform.position;
         transform.position = new Vector2(currentPositionInWorld.x, Mathf.Max(mousePositionInWorld.y + offset.y, stopYPosition));
     }
-    
+
     private void OnMouseDown()
     {
+
+        Debug.Log("Bellows clicked");
+         
         offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition); //sprite location - mouse location (in world)
         fireHold.fire.duration += Time.deltaTime;
+
+        // play sound
+
+          if (audioSource != null && fireCrackleSound != null)
+        {
+            audioSource.clip = fireCrackleSound;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource or fireCrackleSound not set on Bellows.");
+        }
     }
 
     void OnMouseUp()
     {
+        // stop sound
+        if (audioSource && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
         //if rat hits bellows, then duration could be 0.
         //don't want to add that to actions list, so check
-        if(fireHold.fire.duration != 0)
+        if (fireHold.fire.duration != 0)
         {
             Debug.Log("Fire duration: " + fireHold.fire.duration);
             OrderSession.instance.AddAction(new FireAction(fireHold.fire));
